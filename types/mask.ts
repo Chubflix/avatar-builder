b9786e4c66f9f4411d9e3b1e27cd872a8ffebe4b
ricktap <pselge@gmail.com>
2025-11-26 14:37:54 +0100

    add Mask Transient Fields

--- types/mask.ts
+++ types/mask.ts
@@ -1,8 +1,11 @@
+type MaskTransientFields = {
+    base64?: string | null;
+}
+
 export type Mask = {
   id?: string | null; // database id
   storage_path?: string | null;
-  base64?: string | null; // transient, not stored in DB
   width?: number | null;
   height?: number | null;
   info_json?: Record<string, any> | null;
-};
+} & MaskTransientFields;
